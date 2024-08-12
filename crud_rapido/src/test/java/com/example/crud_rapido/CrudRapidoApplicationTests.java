3c7a8103d372a74e77350851cefcfb796324f7ce
Breynersmartinez <153401516+Breynersmartinez@users.noreply.github.com>
2024-08-12 17:23:39 -0500

    Add files via upload

---
+++ crud_rapido/src/test/java/com/example/crud_rapido/CrudRapidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_rapido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRapidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
